4bdc66d33adf3af13bc0f052c359f57387c3e1d8
Taniya Pathak <taniyanawalpathak@gmail.com>
2024-05-12 11:45:43 +0530

    User Input/Output Completed.

---
+++ 1. Learn the Basics/1. User Input-Output/Basic.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    // std::cout << "First Coding Program"
+    //           << "\n";
+    // std::cout << "First Coding Program";
+
+    std::cout << "First Coding Program"
+              << std::endl;
+    std::cout << "First Coding Program";
+    return 0;
+}
---
+++ 1. Learn the Basics/1. User Input-Output/Basic1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello"
+         << "\n";
+    cout << "World";
+    return 0;
+}
